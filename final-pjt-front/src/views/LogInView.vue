cecad9282ecfeb01fafcba6f79b4d7e531f4bf0b
leeSsang96 <lgoda14@naver.com>
2025-05-22 15:15:05 +0900

    양관아미안하다

---
+++ final-pjt-front/src/views/LogInView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>로그인 페이지</h1>
+    <form @submit.prevent="">
+      <input type="text" class="mb-2" placeholder="아이디"> <br>
+      <input type="password" class="mb-2" placeholder="비밀번호"><br>
+      <button>로그인</button>
+      <hr>
+      <button>회원가입</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
