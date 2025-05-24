6c6c454e7e35de8c1c633c1af754afc93430e913
leessang96 <lgoda13@gmail.com>
2025-05-24 17:00:39 +0900

    날아가나

--- final-pjt-back/articles/views.py
+++ final-pjt-back/articles/views.py
@@ -29,10 +29,20 @@ def article_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'DELETE', 'PUT'])
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
-    print('ㅈ댓다;;')
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+    
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
